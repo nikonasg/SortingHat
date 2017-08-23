ce65e2a5f86d3ff2fdeb08b1c5b3e7359fb25bb5
Nik Gianitsos <gianitsos04@icloud.com>
2017-08-22 22:03:28 -0500

    Added questions and structure to print outs

--- HarryPotterQuiz.java
+++ HarryPotterQuiz.java
@@ -17,6 +17,8 @@ public class HarryPotterQuiz {
                 + " Each answer will contribute points to a house with the tribute most like your answer."
                 + " After you are done with the quiz, you will be told the house you most belong to."
                 + " Here is the quiz.");
+        System.out.println();
+        System.out.println();
     }
 
     public static void quiz() {
@@ -24,7 +26,7 @@ public class HarryPotterQuiz {
         int hufflepuff = 0;
         int slytherin = 0;
         int ravenclaw = 0;
-        
+
         Scanner scan = new Scanner(System.in);
         String[] answers1 = {"Bold", "Kind", "Bossy", "Cunning"};
         String[] answers2 = {"Help it", "Alert somebody of it", "Ignore it", "Find out what's wrong with it"};
@@ -32,50 +34,52 @@ public class HarryPotterQuiz {
         String[] answers4 = {"How courages you were", "The friends you had", "How great you were", "Your acheivments"};
         String[] answers5 = {"The dark path iluminated only by light posts", "The sunny fields path", "The stoned road path", "The confusing curvy path"};
         String[][] answers = {answers1, answers2, answers3, answers4, answers5};
-        for (int r = 0; r < answers.length; r++) {
+        String[] questions = {"How would you describe yourself", "What would you do if you found a wounded animal", "What is the first thing you would do with a new spell", "If there was a shortcut to go to hogwarts which one would you choose"};
+        for (int ans = 0; ans < answers.length; ans++) {
+            System.out.println(questions[ans]);
 
-            for (int c = 0; c < answers[r].length; c++) {
-                System.out.println("Choose an answer by writing the number it corresponds to.");
-                int playerChoice = scan.nextInt();
-                scan.nextLine();
-                switch (playerChoice) {
-                    case 0:{
-                        gryffindor++;
-                        break;
-                    }
-                    case 1: {
-                        hufflepuff++;
-                        break;
-                    }
-                    case 2: {
-                        slytherin++;
-                        break;
-                    }
-                    case 3: {
-                        ravenclaw++;
-                        break;
-                    }
-                }
-                
-
+            for (int choice = 0; choice < answers[ans].length; choice++) {
+                System.out.println(answers[ans][choice]);
             }
+            System.out.println();
+            System.out.println("Choose an answer by the order they are placed in.");
+            int playerChoice = scan.nextInt();
+            scan.nextLine();
+            System.out.println();
+
+            switch (playerChoice) {
+                case 0: {
+                    gryffindor++;
+                    break;
+                }
+                case 1: {
+                    hufflepuff++;
+                    break;
+                }
+                case 2: {
+                    slytherin++;
+                    break;
+                }
+                case 3: {
+                    ravenclaw++;
+                    break;
+                }
+            }
+
         }
         String house = "";
-        mostPoints(gryffindor, hufflepuff, slytherin, ravenclaw, house);
-        System.out.println("You were sorted into the " + house + " house!");
+        System.out.println("You were sorted into the " + mostPoints(gryffindor, hufflepuff, slytherin, ravenclaw, house) + " house!");
     }
+
     private static String mostPoints(int gryffindor, int hufflepuff, int slytherin, int ravenclaw, String house) {
         if (gryffindor > hufflepuff && gryffindor > slytherin && gryffindor > ravenclaw) {
-            house = "gryffindor";
-        }
-        else if (hufflepuff > slytherin && hufflepuff > ravenclaw) {
-            house = "hufflepuff";
-        }
-        else if (slytherin > ravenclaw) {
-            house = "slytherin";
-        }
-        else {
-            house = "ravenclaw";
+            house = "Gryffindor";
+        } else if (hufflepuff > slytherin && hufflepuff > ravenclaw) {
+            house = "Hufflepuff";
+        } else if (slytherin > ravenclaw) {
+            house = "Slytherin";
+        } else {
+            house = "Ravenclaw";
         }
         return house;
     }
